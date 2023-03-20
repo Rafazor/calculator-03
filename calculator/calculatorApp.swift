3370f60971f6b3377a4af265d9a48f396c1763cf
Călin Rafa <calin.rafa@alexishr.com>
2023-03-20 17:52:36 +0200

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Calin Rafa on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
